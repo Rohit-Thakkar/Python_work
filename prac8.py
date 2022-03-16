d2da9045d9938f90b9db83d2d5f6329e9cd743d4
Rohit-Thakkar <97794321+Rohit-Thakkar@users.noreply.github.com>
2022-03-16 22:26:28 +0530

    Add files via upload
    Updated prac8

---
+++ prac8.py
@@ -0,0 +1,34 @@
+# 20ce147_prac9
+# LINK https://github.com/Rohit-Thakkar/python_assign_sem4/
+# Aim:Write a Program in Python to implement a Stack Data Structure using Class and Objects, with push, pop, and traversal method.
+class Stack:
+    def __init__(self):
+        self.items = []
+ 
+    def is_empty(self):
+        return self.items == []
+ 
+    def push(self, data):
+        self.items.append(data)
+ 
+    def pop(self):
+        return self.items.pop()
+ 
+ 
+s = Stack()
+while True:
+    print('push value (put a space bettween push and whatever value u need to push)')
+    print('pop')
+    print('quit')
+    do = input('What would you like to do? ').split(" ")
+ 
+    operation = do[0].strip().lower()
+    if operation == 'push':
+        s.push(int(do[1]))
+    elif operation == 'pop':
+        if s.is_empty():
+            print('Stack is empty.')
+        else:
+            print('Popped value: ', s.pop())
+    elif operation == 'quit':
+        break
